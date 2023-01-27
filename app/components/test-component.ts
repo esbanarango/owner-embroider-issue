b33d3c8e852f4d81b38bf5289fcdd8386c508ab9
Esteban Arango Medina <marranoparael31@gmail.com>
2023-01-26 23:20:33 -0500

    tests failing

--- app/components/test-component.ts
+++ app/components/test-component.ts
@@ -1,5 +1,5 @@
 import Component from '@glimmer/component';
 
-export default class TestComponent extends Component {
+export default class TestComponentComponent extends Component {
   color = 'green';
 }
