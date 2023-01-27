cf9cfdd4dfa1b4e1b832677c00e5d7fe5c6f4e8f
Esteban Arango Medina <marranoparael31@gmail.com>
2023-01-26 22:49:25 -0500

    ember init (with embroider)

---
+++ tests/test-helper.js
@@ -0,0 +1,12 @@
+import Application from 'owner-embroider-issue/app';
+import config from 'owner-embroider-issue/config/environment';
+import * as QUnit from 'qunit';
+import { setApplication } from '@ember/test-helpers';
+import { setup } from 'qunit-dom';
+import { start } from 'ember-qunit';
+
+setApplication(Application.create(config.APP));
+
+setup(QUnit.assert);
+
+start();
